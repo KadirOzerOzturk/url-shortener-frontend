7663ca97be1d47523fe0a4eb81b65712ebbb3f6c
KadirOzerOzturk <ozturkkadirozer@gmail.com>
2025-02-28 20:20:30 +0300

    feat: add react-router-sitemap dependency and update ads.txt for ad configuration; adjust GoogleAds component layout

--- src/App.js
+++ src/App.js
@@ -22,10 +22,11 @@ function App() {
         </Routes>
 
         {/* Reklamları sağa ve sola ekleyelim */}
-        <div className="absolute top-1/2 left-0 transform -translate-y-1/2 ml-4">
+        <div className="absolute top-1/2 left-0 transform -translate-y-1/2 ml-4 w-1/12">
+
           <GoogleAds />
         </div>
-        <div className="absolute top-1/2 right-0 transform -translate-y-1/2 mr-4">
+        <div className="absolute top-1/2 right-0 transform -translate-y-1/2 mr-4 w-1/12">
           <GoogleAds />
         </div>
       </div>
