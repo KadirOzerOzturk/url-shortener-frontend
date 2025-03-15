8c6b26b1b474eded229a6f9550fc8971093e2d02
KadirOzerOzturk <ozturkkadirozer@gmail.com>
2025-03-15 16:33:12 +0300

    fix: remove unused import

--- src/components/Redirect.jsx
+++ src/components/Redirect.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Helmet } from "react-helmet";
-import Footer from "../components/Footer"; // Footer'ı içe aktar
 
 const Redirect = () => {
   const { shortUrl } = useParams();
